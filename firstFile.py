dbd6daa0abb61e7f6d53102bbc2c1b3ac568d36b
Bryan Keating <ru.bryankeating@outlook.com>
2024-02-02 22:55:02 -0500

    Added Features

--- firstFile.py
+++ firstFile.py
@@ -6,10 +6,16 @@ first_list = ["bryan",21,"emma",19,"owen",17]
 print("I will begin changes to learn how git\
  stores version history")
 
-holdVar = 4
+holdVar = 3
 for i in range(holdVar):
 	print("iteration" + str(i))
 
 import math
-x = math.exp(5)
+x = math.exp(6)
 print(x)
+
+print("This is the first line of the new branch\
+labeled 'New-Features'")
+print("For this first test I am only going to add lines of \
+code, not delete")
+print("I also changed the number in line 14 from 5 to 6")
