2b2320a656a731292de71f7c496bf06feb85b0b8
Bryan Keating <ru.bryankeating@outlook.com>
2024-02-02 22:43:54 -0500

    First Commit

---
+++ firstFile.py
@@ -0,0 +1,15 @@
+print("Hello World")
+firstVar = 5
+first_dict = {"element 1":"value1","key 2":"value2"}
+first_list = ["bryan",21,"emma",19,"owen",17]
+
+print("I will begin changes to learn how git\
+ stores version history")
+
+holdVar = 4
+for i in range(holdVar):
+	print("iteration" + str(i))
+
+import math
+x = math.exp(5)
+print(x)
